91eac4479b41fa5735245ad9949623b1661b0963
Tonyniu <tonyniu@lagou.com>
2016-12-27 18:38:31 +0800

    修改了一下实现方式，但还是会闪，待找到更好的方法

--- app/src/main/java/com/wzhnsc/testcustomtopbarshowhide/MainActivity.java
+++ app/src/main/java/com/wzhnsc/testcustomtopbarshowhide/MainActivity.java
@@ -4,30 +4,22 @@ import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.RelativeLayout;
 
+import java.util.Locale;
+
 public class MainActivity extends Activity {
 
     private RelativeLayout mrlTopBar;
     private MyWebView      mWebView;
 
-    // 操作栏渐变处理标识
-    private boolean bIsUpdate = true;
-
-    // 现在网页向下滑动标识
-    private boolean bIsNowGoDown = false;
-    // 之前网页向下滑动标识
-    private boolean bIsPreGoDown = false;
-
     private float mImageAlpha = 1.0f;
     private float mWillAddAlpha = 0.1f;
-
-    private Handler mHandler;
 
     private class MyWebViewClient extends WebViewClient {
         @Override
@@ -45,67 +37,48 @@ public class MainActivity extends Activity {
         @Override
         public void onPageFinished(WebView view, String url) {
         }
-
-        @Override
-        public void onReceivedError(WebView view, int errorCode,String description, String failingUrl) {
-            super.onReceivedError(view, errorCode, description, failingUrl);
-        }
     }
 
-    private void titleBarShowOrHide(boolean IsVisible) {
+    private static int mLastAlphaValue = 255;
+    private static int mAlphaValue = 255;
+    private void titleBarShowOrHide(boolean isUp, int shiftValue) {
+        String strLog = String.format(Locale.getDefault(),
+                "titleBarShowOrHide - mWebView = %s",
+                mWebView.toString());
+
+        Log.w("wzhnsc", strLog);
+
         if (null != mWebView
          && mWebView.canGoBack()) {
             return;
         }
 
-        if (IsVisible) {
-            mWillAddAlpha = 0.1f;
-            mrlTopBar.setVisibility(View.VISIBLE);
-        }
-        else {
-            mWillAddAlpha = -0.1f;
-        }
+        mAlphaValue = 255 - shiftValue;
 
-        bIsUpdate = false;
+        mAlphaValue = (0 > mAlphaValue) ? 0 : mAlphaValue;
 
-        updateAlpha();
-    }
+        strLog = String.format(Locale.getDefault(),
+                "titleBarShowOrHide(isUp = %b, shiftValue = %d), mAlphaValue = %d",
+                isUp, shiftValue, mAlphaValue);
 
-    public void updateAlpha() {
-        if (bIsUpdate) {
-            return;
-        }
+        Log.w("wzhnsc", strLog);
 
-        mImageAlpha += mWillAddAlpha;
-
-        if (mImageAlpha > 1.0f) {
-            mImageAlpha = 1.0f;
-        }
-
-        if (mImageAlpha < 0.0f) {
-            mImageAlpha = 0.0f;
-        }
-
-        if (null != mHandler) {
-            mHandler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    mrlTopBar.setAlpha(mImageAlpha);
-
-                    if (mImageAlpha == 0) {
-                        mrlTopBar.setVisibility(View.GONE);
-                        bIsUpdate = true;
-                    }
-
-                    if (mImageAlpha == 255) {
-                        bIsUpdate = true;
-                    }
-
-                    updateAlpha();
+        if (mLastAlphaValue != mAlphaValue) {
+            if (isUp) {
+                if (0 == mAlphaValue) {
+                    mrlTopBar.setVisibility(View.GONE);
                 }
+            }
+            else {
+                mrlTopBar.setVisibility(View.VISIBLE);
+            }
 
-            }, 80);
+            mrlTopBar.setAlpha(mImageAlpha);
+
+            getWindow().getDecorView().postInvalidate();
         }
+
+        mLastAlphaValue = mAlphaValue;
     }
 
     @Override
@@ -113,8 +86,6 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-
-        mHandler = new Handler();
 
         mrlTopBar = (RelativeLayout)findViewById(R.id.rl_top_bar);
 
@@ -127,24 +98,22 @@ public class MainActivity extends Activity {
         mWebView.setScrollChangedListener(new MyWebView.WebViewScroll() {
             @Override
             public void dealScrollChanged(int l, int t, int oldl, int oldt) {
-                if (t > oldt) {
-                    bIsPreGoDown = bIsNowGoDown;
-                    bIsNowGoDown = true;
-                } else {
-                    bIsPreGoDown = bIsNowGoDown;
-                    bIsNowGoDown = false;
-                }
+                String strLog = String.format(Locale.getDefault(),
+                                              "dealScrollChanged(l = %d, t = %d, oldl = %d, oldt = %d)",
+                                              l, t, oldl, oldt);
 
-                if (bIsPreGoDown != bIsNowGoDown) {
-                    titleBarShowOrHide(!bIsNowGoDown);
-                }
+                Log.w("wzhnsc", strLog);
+
+                // 纵向：正值向上滑动，负值向下滑动
+                titleBarShowOrHide(((t - oldt) > 0), t);
             }
         });
+
         mWebView.setWebViewClient(new MyWebViewClient());
-        mWebView.loadUrl("http://www.lagou.com");
+        mWebView.loadUrl("https://github.com/wzhnsc");
 
         WebSettings webSettings = mWebView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
+        //webSettings.setJavaScriptEnabled(true);
         webSettings.setSupportZoom(true);
     }
 
